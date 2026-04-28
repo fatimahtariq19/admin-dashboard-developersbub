5b1d3eb527421db8e303161ed0629cddeea7f7e5
fatimahtariq19 <fatimahtariq687@gmail.com>
2026-04-28 21:22:18 +0500

    little modification

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Routes>
 
         {/* PUBLIC */}
-        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Login />} /> */}
 
         {/* PROTECTED */}
         <Route
